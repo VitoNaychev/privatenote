1fc218843ee8a562f105fddac1c2492631c239b6
root <vitonaychev@gmail.com>
2017-11-05 18:17:52 +0200

    neshta

--- app/controllers/create_message_controller.rb
+++ app/controllers/create_message_controller.rb
@@ -1,21 +1,77 @@
 #$uid = Array.new
+require 'json'
+require 'nokogiri'
 
 class CreateMessageController < ApplicationController
+    protect_from_forgery with: :null_session
+    
     MIN_RND = 1000
     MAX_RND = 9999
-    
+        
     def index
     end
     
     def create
         
-        $cur_uid = get_free_uid.to_s
+        cur_uid = get_free_uid.to_s
         text = params[:text]
         puts text
-        Database.create(:uid => $cur_uid, :text => text) 
+        Database.create(:uid => cur_uid, :text => text) 
         #redirect_back(fallback_location: "create_message")
-        render plain: "https://private-note.herokuapp.com/read_message/#{$cur_uid}"
+        url =  "https://private-note.herokuapp.com/create_message/#{cur_uid}"
+        render plain: url
     end
+    
+    def read
+        id = params[:id]
+        data_rec = Database.find_by uid: id.to_s 
+        if data_rec.class == NilClass
+            render_404
+            #render :status => 404
+            #raise ActionController::RoutingError.new('Not Found') 
+            #$read_text = "DUM FUK"
+        else
+            $read_text = data_rec.send(:text)
+            read_id = data_rec.send(:id)
+            Database.destroy(read_id.to_i)
+        end
+        #Database.destroy(id)
+
+        #if $read_text.equals?(nil)
+            
+    end
+
+    def api
+        
+        if request.headers["Content-Type"] == 'application/json'
+            text = params[:message].to_s
+        elsif request.headers["Content-Type"] == 'text/xml'
+            text = Nokogiri::XML.fragment(request.body.read).content.to_s
+        end
+
+        p text
+        
+        cur_uid = get_free_uid.to_s
+        Database.create(:uid => cur_uid, :text => text) 
+        
+        url =  "https://private-note.herokuapp.com/create_message/#{cur_uid}"       
+        
+        response = {"url" => url}
+       
+        if request.headers["Content-Type"] == 'application/json'
+            render json: response
+        elsif request.headers["Content-Type"] == 'text/xml'
+            render xml: response
+        end
+    end
+
+    def render_404
+      respond_to do |format|
+      format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
+      format.xml  { head :not_found }
+      format.any  { head :not_found }
+      end
+   end
 
     def get_free_uid
         loop do
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
     resources :databases
   
-    get '/read_message/:id' => 'read_message#read' 
+    #get '/read_message/:id' => 'read_message#read' 
+    get 'create_message/:id' => 'create_message#read' 
     get 'create_message' => 'create_message#index'
     post 'create_message' => 'create_message#create'
-  
+    post 'messages/api' => 'create_message#api'
+     
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
